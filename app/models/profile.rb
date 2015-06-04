c9ff67da6e93a058caf91dc02f70557f8f4cff99
JANICE MIN <JANICEMIN@GMAIL.COM>
2015-06-04 13:45:17 -0400

    trying to add photos

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 	has_many :friendships, dependent: :destroy #joined table 
 	has_many :friends, through: :friendships 
+	has_one :picture, :as => :imageable
 
 	def possible_friends
 		Profile.all - friends - [self]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603025241) do
+ActiveRecord::Schema.define(version: 20150604173841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,16 @@ ActiveRecord::Schema.define(version: 20150603025241) do
 
   add_index "mailboxer_receipts", ["notification_id"], name: "index_mailboxer_receipts_on_notification_id", using: :btree
   add_index "mailboxer_receipts", ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type", using: :btree
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "imageable_id"
+    t.string   "imageable_type"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "pictures", ["imageable_type", "imageable_id"], name: "index_pictures_on_imageable_type_and_imageable_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
